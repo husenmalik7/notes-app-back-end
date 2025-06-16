43501a9cf6c795aef1c5039347861c3570db3e08
Husen <husenmalik7@gmail.com>
2025-06-16 18:35:09 +0700

    add generate refresh token

--- src/tokenize/TokenManager.js
+++ src/tokenize/TokenManager.js
@@ -1,8 +1,10 @@
 const Jwt = require('@hapi/jwt');
 const tokenKey = process.env.ACCESS_TOKEN_KEY;
+const refreshKey = process.env.REFRESH_TOKEN_KEY;
 
 const TokenManager = {
   generateAccessToken: (payload) => Jwt.token.generate(payload, tokenKey),
+  generateRefreshToken: (payload) => Jwt.token.generate(payload, refreshKey),
 };
 
 module.exports = TokenManager;
