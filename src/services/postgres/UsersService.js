c83f7fcaee486f8313a22dd8de4e32c725877c9e
Husen <husenmalik7@gmail.com>
2025-06-17 07:51:57 +0700

    fix error

--- src/services/postgres/UsersService.js
+++ src/services/postgres/UsersService.js
@@ -1,5 +1,5 @@
-const { nanoid } = require('nanoid');
 const { Pool } = require('pg');
+const { nanoid } = require('nanoid');
 const bcrypt = require('bcrypt');
 const InvariantError = require('../../exceptions/InvariantError');
 const NotFoundError = require('../../exceptions/NotFoundError');
@@ -8,29 +8,6 @@ const AuthenticationError = require('../../exceptions/AuthenticationError');
 class UsersService {
   constructor() {
     this._pool = new Pool();
-  }
-
-  async verifyUserCredential(username, password) {
-    const query = {
-      text: 'SELECT id, password FROM users WHERE username = $1',
-      values: [username],
-    };
-
-    const result = await this._pool.query(query);
-
-    if (!result.rows.length) {
-      throw new AuthenticationError('Kredensial yang Anda berikan salah');
-    }
-
-    const { id, password: hashedPassword } = result.rows[0];
-
-    const match = await bcrypt.compare(password, hashedPassword);
-
-    if (!match) {
-      throw new AuthenticationError('Kredensial yang Anda berikan salah');
-    }
-
-    return id;
   }
 
   async addUser({ username, password, fullname }) {
@@ -80,6 +57,29 @@ class UsersService {
     }
 
     return result.rows[0];
+  }
+
+  async verifyUserCredential(username, password) {
+    const query = {
+      text: 'SELECT id, password FROM users WHERE username = $1',
+      values: [username],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new AuthenticationError('Kredensial yang Anda berikan salah');
+    }
+
+    const { id, password: hashedPassword } = result.rows[0];
+
+    const match = await bcrypt.compare(password, hashedPassword);
+
+    if (!match) {
+      throw new AuthenticationError('Kredensial yang Anda berikan salah');
+    }
+
+    return id;
   }
 }
 
