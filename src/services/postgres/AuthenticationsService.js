b955cccb0e207890813b77b9b0041afa45470119
Husen <husenmalik7@gmail.com>
2025-06-17 07:54:27 +0700

    fix errror

--- src/services/postgres/AuthenticationsService.js
+++ src/services/postgres/AuthenticationsService.js
@@ -8,7 +8,7 @@ class AuthenticationsService {
 
   async addRefreshToken(token) {
     const query = {
-      text: `INSERT INTO authentications  VALUES($1)`,
+      text: 'INSERT INTO authentications VALUES($1)',
       values: [token],
     };
 
@@ -17,7 +17,7 @@ class AuthenticationsService {
 
   async verifyRefreshToken(token) {
     const query = {
-      text: `SELECT token FROM authentications WHERE token = $1`,
+      text: 'SELECT token FROM authentications WHERE token = $1',
       values: [token],
     };
 
@@ -29,8 +29,11 @@ class AuthenticationsService {
   }
 
   async deleteRefreshToken(token) {
+    // TODO
+    // await this.verifyRefreshToken(token);
+
     const query = {
-      text: `DELETE FROM authentications WHERE token = $1`,
+      text: 'DELETE FROM authentications WHERE token = $1',
       values: [token],
     };
 
