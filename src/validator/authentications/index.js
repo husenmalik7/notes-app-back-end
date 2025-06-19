96da7b96b54289fe1a49ba7ce73cef4488666837
Husen <husenmalik7@gmail.com>
2025-06-19 09:28:27 +0700

    change validate to validation in validators

--- src/validator/authentications/index.js
+++ src/validator/authentications/index.js
@@ -7,23 +7,21 @@ const InvariantError = require('../../exceptions/InvariantError');
 
 const AuthenticationsValidator = {
   validatePostAuthenticationPayload: (payload) => {
-    const validateResult = PostAuthenticationPayloadSchema.validate(payload);
-    if (validateResult.error) {
-      throw new InvariantError(validateResult.error.message);
+    const validationResult = PostAuthenticationPayloadSchema.validate(payload);
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message);
     }
   },
-
   validatePutAuthenticationPayload: (payload) => {
-    const validateResult = PutAuthenticationPayloadSchema.validate(payload);
-    if (validateResult.error) {
-      throw new InvariantError(validateResult.error.message);
+    const validationResult = PutAuthenticationPayloadSchema.validate(payload);
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message);
     }
   },
-
   validateDeleteAuthenticationPayload: (payload) => {
-    const validateResult = DeleteAuthenticationPayloadSchema.validate(payload);
-    if (validateResult.error) {
-      throw new InvariantError(validateResult.error.message);
+    const validationResult = DeleteAuthenticationPayloadSchema.validate(payload);
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message);
     }
   },
 };
--- src/validator/notes/index.js
+++ src/validator/notes/index.js
@@ -3,9 +3,9 @@ const { NotePayloadSchema } = require('./schema');
 
 const NotesValidator = {
   validateNotePayload: (payload) => {
-    const validateResult = NotePayloadSchema.validate(payload);
-    if (validateResult.error) {
-      throw new InvariantError(validateResult.error.message);
+    const validationResult = NotePayloadSchema.validate(payload);
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message);
     }
   },
 };
