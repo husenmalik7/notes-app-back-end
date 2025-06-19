635ab51d504d507aae502edee9c709d263787100
Husen <husenmalik7@gmail.com>
2025-06-19 09:30:14 +0700

    docs: token

--- src/tokenize/TokenManager.js
+++ src/tokenize/TokenManager.js
@@ -1,22 +1,20 @@
 const Jwt = require('@hapi/jwt');
 const InvariantError = require('../exceptions/InvariantError');
-const tokenKey = process.env.ACCESS_TOKEN_KEY;
-const refreshKey = process.env.REFRESH_TOKEN_KEY;
 
 const TokenManager = {
-  generateAccessToken: (payload) => Jwt.token.generate(payload, tokenKey),
-  generateRefreshToken: (payload) => Jwt.token.generate(payload, refreshKey),
+  generateAccessToken: (payload) => Jwt.token.generate(payload, process.env.ACCESS_TOKEN_KEY),
+  generateRefreshToken: (payload) => Jwt.token.generate(payload, process.env.REFRESH_TOKEN_KEY),
   verifyRefreshToken: (refreshToken) => {
     try {
       const artifacts = Jwt.token.decode(refreshToken);
-      Jwt.token.verifySignature(artifacts, refreshKey);
-
+      Jwt.token.verifySignature(artifacts, process.env.REFRESH_TOKEN_KEY);
       const { payload } = artifacts.decoded;
       return payload;
     } catch (error) {
       throw new InvariantError('Refresh token tidak valid');
     }
   },
+
 };
 
 module.exports = TokenManager;
