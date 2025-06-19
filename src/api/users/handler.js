9f8da9a9d7635a0c631317eafe133ff4564607ff
Husen <husenmalik7@gmail.com>
2025-06-19 12:06:11 +0700

    123

--- src/api/users/handler.js
+++ src/api/users/handler.js
@@ -1,3 +1,5 @@
+const ClientError = require('../../exceptions/ClientError');
+
 class UsersHandler {
   constructor(service, validator) {
     this._service = service;
@@ -34,6 +36,38 @@ class UsersHandler {
         user,
       },
     };
+  };
+
+  getUsersByUsernameHandler = async (request, h) => {
+    try {
+      const { username = '' } = request.query;
+      const users = await this._service.getUsersByUsername(username);
+
+      return {
+        status: 'success',
+        data: {
+          users,
+        },
+      };
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: 'fail',
+          message: error.message,
+        });
+        response.code(error.statusCode);
+        return response;
+      }
+
+      //  Server ERROR!
+      const response = h.response({
+        status: 'error',
+        message: 'Maaf, terjadi kegagalan pada server kami.',
+      });
+      response.code(500);
+      console.error(error);
+      return response;
+    }
   };
 }
 
--- src/services/postgres/UsersService.js
+++ src/services/postgres/UsersService.js
@@ -36,7 +36,9 @@ class UsersService {
     const result = await this._pool.query(query);
 
     if (result.rows.length > 0) {
-      throw new InvariantError('Gagal menambahkan user. Username sudah digunakan.');
+      throw new InvariantError(
+        'Gagal menambahkan user. Username sudah digunakan.'
+      );
     }
   }
 
@@ -76,6 +78,17 @@ class UsersService {
     }
 
     return id;
+  }
+
+  async getUsersByUsername(username) {
+    const query = {
+      text: 'SELECT id, username, fullname FROM users WHERE username LIKE $1',
+      values: [`%${username}%`],
+    };
+
+    const result = await this._pool.query(query);
+
+    return result.rows;
   }
 }
 
